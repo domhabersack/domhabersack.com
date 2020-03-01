dcdfcf41ec0fec72eeee7431c1b62207aaa86133
Dom Habersack <dom@islovely.co>
2020-03-01 12:06:25 +0100

    Remove unnecessary semicolons

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,7 +6,7 @@ import Footer from './footer'
 import Header from './header'
 import Taper from './taper'
 
-const IS_IN_DEBUG_MODE = false;
+const IS_IN_DEBUG_MODE = false
 
 export default ({ breadcrumbs, children }) => (
   <>
