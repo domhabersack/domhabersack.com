c84d626341e1165961b86e1d4eeb3b375fd3a1e2
Dom Habersack <dom@islovely.co>
2021-03-23 13:22:07 +0100

    Remove whitespace

--- src/components/meta-tags.js
+++ src/components/meta-tags.js
@@ -55,7 +55,6 @@ export default function MetaTags({
           {updatedAt && (<meta property="article:modified_time" content={updatedAt} />)}
         </React.Fragment>
       )}
-
     </Head>
   )
 }
