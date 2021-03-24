b3a762b44f5f9bdd98c5f227582f1676e845dbb3
Dom Habersack <dom@islovely.co>
2021-03-24 19:27:44 +0100

    Repeat same selective import for api-helpers

--- src/lib/api-helpers.js
+++ src/lib/api-helpers.js
@@ -1,12 +1,12 @@
-import fs from 'fs'
+import { existsSync, readdirSync, readFileSync } from 'fs'
+import { join } from 'path'
 import matter from 'gray-matter'
-import path from 'path'
 
 export async function getAllFiles(type, transform = () => {}) {
   return getSlugs(type).reduce(async (previousPromise, slug) => {
     const allFiles = await previousPromise
 
-    const file = fs.readFileSync(path.join(process.cwd(), `_${type}/${slug}/index.mdx`), 'utf8')
+    const file = readFileSync(join(process.cwd(), `_${type}/${slug}/index.mdx`), 'utf8')
 
     const {
       data: frontmatter,
@@ -28,7 +28,7 @@ export async function getAllFiles(type, transform = () => {}) {
 }
 
 export async function getFileBySlug(type, slug, transform = () => {}) {
-  const file = fs.readFileSync(path.join(process.cwd(), `_${type}/${slug}/index.mdx`), 'utf8')
+  const file = readFileSync(join(process.cwd(), `_${type}/${slug}/index.mdx`), 'utf8')
 
   const {
     content,
@@ -69,10 +69,10 @@ export async function getFileBySlug(type, slug, transform = () => {}) {
 }
 
 export function getSlugs(type) {
-  return fs.readdirSync(path.join(process.cwd(), `_${type}`), {
+  return readdirSync(join(process.cwd(), `_${type}`), {
     withFileTypes: true,
   }).filter(dirent => dirent.isDirectory()).map(({ name }) => name).filter(slug => {
     // keep only if directory contains an `index.mdx`
-    return fs.existsSync(path.join(process.cwd(), `_${type}/${slug}/index.mdx`))
+    return existsSync(join(process.cwd(), `_${type}/${slug}/index.mdx`))
   })
 }
