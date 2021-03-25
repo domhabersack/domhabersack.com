b7ec7a661f51ed00773030ed18b3342f2d38f38a
Dom Habersack <dom@islovely.co>
2021-03-25 14:07:12 +0100

    Import full packages again

--- pages/api/[type]/[slug]/[name].js
+++ pages/api/[type]/[slug]/[name].js
@@ -1,5 +1,5 @@
-import { readFileSync } from 'fs'
-import { extname, join } from 'path'
+import fs from 'fs'
+import path from 'path'
 
 const ALLOWED_TYPES = [
   'authors',
@@ -40,14 +40,14 @@ export default function handler(req, res) {
     res.status(404)
   }
 
-  const filePath = join(process.cwd(), `_${type}`, slug, name)
-  const extension = extname(filePath)
+  const filePath = path.join(process.cwd(), `_${type}`, slug, name)
+  const extension = path.extname(filePath)
 
   if (!ALLOWED_EXTENSIONS.includes(extension)) {
     res.status(404)
   }
 
-  const file = readFileSync(filePath)
+  const file = fs.readFileSync(filePath)
 
   res.setHeader('Content-Type', CONTENT_TYPE_BY_EXTENSION[extension])
   res.end(file)
