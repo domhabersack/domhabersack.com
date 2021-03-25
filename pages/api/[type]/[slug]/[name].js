25251a7ac6f8eea795ab523a9370ab91bcad5e3d
Dom Habersack <dom@islovely.co>
2021-03-25 14:32:10 +0100

    I don’t even know if I can do this with require

--- pages/api/[type]/[slug]/[name].js
+++ pages/api/[type]/[slug]/[name].js
@@ -1,5 +1,5 @@
-const fs = require('fs').promises
-const path = require('path')
+const { readFile } = require('fs').promises
+const { join, extname } = require('path')
 
 const ALLOWED_TYPES = [
   'authors',
@@ -40,14 +40,14 @@ export default async function handler(req, res) {
     res.status(404)
   }
 
-  const filePath = path.join(process.cwd(), `_${type}`, slug, name)
-  const extension = path.extname(filePath)
+  const filePath = join(process.cwd(), `_${type}`, slug, name)
+  const extension = extname(filePath)
 
   if (!ALLOWED_EXTENSIONS.includes(extension)) {
     res.status(404)
   }
 
-  const file = await fs.readFile(filePath)
+  const file = await readFile(filePath)
 
   res.setHeader('Content-Type', CONTENT_TYPE_BY_EXTENSION[extension])
   res.end(file)
