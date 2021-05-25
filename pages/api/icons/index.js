732f01e9f864662ca3d87889e1cb9a2c93fc984a
Dom Habersack <dom@domhabersack.com>
2021-05-25 17:19:16 +0200

    Do positive checks in icons endpoint instead of negative ones
    This is easier to read and doesn’t need additional `return` statements
    everywhere. Less code means fewer opportunities for mistakes.

--- pages/api/icons/index.js
+++ pages/api/icons/index.js
@@ -3,41 +3,36 @@ import path from 'path'
 import { parse } from 'svg-parser'
 
 export default function handler(req, res) {
-  if (req.method !== 'GET') {
-    res.status(404)
-    res.end
-  }
+  if (req.method === 'GET') {
+    const {
+      query: {
+        name,
+        size,
+        style,
+      },
+    } = req
 
-  const {
-    query: {
-      name,
-      size,
-      style,
-    },
-  } = req
+    const isSmall = size === 'small'
+    const isSolid = style === 'solid'
 
-  const isSmall = size === 'small'
-  const isSolid = style === 'solid'
+    const filePath = path.join(process.cwd(), 'src', 'icons', `${name}${isSmall ? '--small' : ''}${isSolid ? '--solid' : ''}.svg`)
 
-  const filePath = path.join(process.cwd(), 'src', 'icons', `${name}${isSmall ? '--small' : ''}${isSolid ? '--solid' : ''}.svg`)
+    if (fs.existsSync(filePath)) {
+      const file = fs.readFileSync(filePath, 'utf8')
+      const ast = parse(file)
 
+      const [root] = ast.children
+      const [child] = root.children
 
-  if (!fs.existsSync(filePath)) {
-    console.error({ filePath })
-    res.status(404)
+      res.json({
+        path: child.properties.d,
+        viewBox: root.properties.viewBox,
+      })
+    } else {
+      console.error({ filePath })
+      res.status(404)
+    }
+
     res.end()
-    return
   }
-
-  const file = fs.readFileSync(filePath, 'utf8')
-  const ast = parse(file)
-
-  const [root] = ast.children
-  const [child] = root.children
-
-  res.json({
-    path: child.properties.d,
-    viewBox: root.properties.viewBox,
-  })
-  res.end()
 }
