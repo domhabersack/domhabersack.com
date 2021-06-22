21be7ec385164d049c9df8bdf48b2a4dd3cbcc01
Dom Habersack <dom@domhabersack.com>
2021-06-22 13:49:54 +0200

    Update styling on milestone log

--- src/components/card.js
+++
@@ -1,9 +0,0 @@
-export default function Card({
-  children,
-}) {
-  return (
-    <div className="bg-white overflow-hidden rounded-md shadow-md dark:bg-gray-900">
-      {children}
-    </div>
-  )
-}
--- src/components/milestone.js
+++ src/components/milestone.js
@@ -20,57 +20,65 @@ export default function Milestone({
   } = embedded
 
   return (
-    <div className="flex items-start space-x-2.5">
-      <div className="bg-yellow-300 p-2 rounded-full dark:bg-yellow-400">
+    <div className="flex items-start space-x-4">
+      <div className="bg-yellow-300 p-3 rounded-md dark:bg-yellow-400">
         <Icon className="h-6 text-gray-700 w-6" type={icon} />
       </div>
 
       <div className="flex-1">
-        <header className="flex flex-col-reverse flex-1 justify-between mb-1 lg:flex-row lg:items-center lg:space-x-2.5">
-          <h3 className="leading-snug m-0 text-base">
+        <div className="space-y-2">
+          <span className="text-gray-500 text-sm dark:text-gray-400">
+            {formatDate(createdAt)}
+          </span>
+
+          <h3 className="font-medium m-0 text-lg">
             {title}
           </h3>
 
-          <span className="text-gray-500 text-xs dark:text-gray-400">
-            {formatDate(createdAt)}
-          </span>
-        </header>
-
-        <p className="m-0 text-sm">
-          {excerpt}
-        </p>
+          <p className="m-0 text-base text-gray-500">
+            {excerpt}
+          </p>
+        </div>
 
         {newsletter && (
-          <div className="mt-3">
+          <div className="mt-4">
             <Card>
-              <div className="px-4 py-3">
-                <h4 className="font-bold mb-0.5 text-sm">
-                  <a href={newsletter.permalink}>
+              <div className="px-4 py-3 space-y-2">
+                <h4 className="font-medium text-base">
+                  <a className="text-gray-900 visited:text-gray-900" href={newsletter.permalink}>
                     {newsletter.title}
                   </a>
                 </h4>
 
-                <p className="m-0 text-sm">
+                <p className="m-0 text-gray-500 text-sm">
                   {newsletter.excerpt}
                 </p>
+
+                <a className="font-medium inline-block text-blue-600 text-sm visited:text-blue-600" href={newsletter.permalink}>
+                  Read full article
+                </a>
               </div>
             </Card>
           </div>
         )}
 
         {project && (
-          <div className="mt-3">
+          <div className="mt-4">
             <Card>
-              <div className="px-4 py-3">
-                <h4 className="font-bold mb-0.5 text-sm">
-                  <a href={project.permalink}>
+              <div className="px-4 py-3 space-y-2">
+                <h4 className="font-medium text-base">
+                  <a className="text-gray-900 visited:text-gray-900" href={project.permalink}>
                     {project.title}
                   </a>
                 </h4>
 
-                <p className="m-0 text-sm">
+                <p className="m-0 text-gray-500 text-sm">
                   {project.excerpt}
                 </p>
+
+                <a className="font-medium inline-block text-blue-600 text-sm visited:text-blue-600" href={project.permalink}>
+                  Read case study
+                </a>
               </div>
             </Card>
           </div>
